a359c928b16358c377d3c2a137f6ecea95089303
Andrey Popov <palanelarty@gmail.com>
2024-06-01 13:43:25 +0300

    fix

--- surService/urls.py
+++ surService/urls.py
@@ -6,7 +6,7 @@ from surApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('show_votings/<int:user_id>/', VotingListByUserAPIView.as_view(), name='voting-list-by-user'),
     path('create/voting/', VotingCreateAPIView.as_view(), name='voting-create'),
     path('update/voting/<int:pk>/', VotingUpdateAPIView.as_view(), name='voting-update'),
@@ -19,4 +19,8 @@ urlpatterns = [
     path('api/detail-statistic/<int:pk>/', DetailStatisticAPIView.as_view(), name='detail-statistic'),
     path('api/poll-statistic/<int:poll_id>/', PollDetailAPIView.as_view(), name='poll-statistic'),
     path('api/add_logic/<int:pk>/', VotingUpdateLogicView.as_view(), name='add-logic'),
+    path('api/submit/<int:pk>/', VotingUpdateSubmitView.as_view(), name='submit'),
+    path('api/token/validate/', ValidateTokenView.as_view(), name='token_validate'),
+    path('api/export-votes/<int:voting_id>/', ExportVotesToExcelView.as_view(), name='export-votes'),
+    path('api/export-voting-json/<int:voting_id>/', ExportVotingToJsonFileView.as_view(), name='export-voting-json'),
 ]
